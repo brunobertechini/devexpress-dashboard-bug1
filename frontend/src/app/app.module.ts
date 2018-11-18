3a8d4af4b9fa1d00b7abec6d04dbce0997e05b75
Bruno Bertechini <bruno.bertechini@outlook.com>
2018-11-18 21:36:25 -0200

    Added empty components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DashboardDesignerComponent } from './dashboard-designer/dashboard-designer.component';
+import { DashboardViewerComponent } from './dashboard-viewer/dashboard-viewer.component';
+import { ReportDesignerComponent } from './report-designer/report-designer.component';
+import { ReportViewerComponent } from './report-viewer/report-viewer.component';
+import { PivotGridComponent } from './pivot-grid/pivot-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardDesignerComponent,
+    DashboardViewerComponent,
+    ReportDesignerComponent,
+    ReportViewerComponent,
+    PivotGridComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/report-designer/report-designer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-report-designer',
+  templateUrl: './report-designer.component.html',
+  styleUrls: ['./report-designer.component.css']
+})
+export class ReportDesignerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
