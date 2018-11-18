0d82f15fb7bab274143aaf0ac11659777804798c
Bruno Bertechini <bruno.bertechini@outlook.com>
2018-11-18 21:58:35 -0200

    Added home component and menu links

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DashboardDesignerComponent } from './dashboard-designer/dashboard-designer.component';
@@ -8,6 +8,16 @@ import { DashboardViewerComponent } from './dashboard-viewer/dashboard-viewer.co
 import { ReportDesignerComponent } from './report-designer/report-designer.component';
 import { ReportViewerComponent } from './report-viewer/report-viewer.component';
 import { PivotGridComponent } from './pivot-grid/pivot-grid.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'dashboard-designer', component: DashboardDesignerComponent },
+  { path: 'dashboard-viewer', component: DashboardViewerComponent },
+  { path: 'report-designer', component: ReportDesignerComponent },
+  { path: 'report-viewer', component: ReportViewerComponent },
+  { path: 'pivot-grid', component: PivotGridComponent }
+];
 
 @NgModule({
   declarations: [
@@ -16,11 +26,13 @@ import { PivotGridComponent } from './pivot-grid/pivot-grid.component';
     DashboardViewerComponent,
     ReportDesignerComponent,
     ReportViewerComponent,
-    PivotGridComponent
+    PivotGridComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
